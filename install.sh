10f1382bfe72462a5f42ef7c3bb49f8f0df56fa4
Gareth Rushgrove <gareth@morethanseven.net>
2015-08-18 12:39:43 +0100

    Support new Puppet 4 paths

--- install.sh
+++ install.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-find skeleton -type f | git checkout-index --stdin --force --prefix="$HOME/.puppet/var/puppet-module/" --
+if [ $(puppet --version |sed -e 's/\..*$//' ) -gt 3 ]; then
+  find skeleton -type f | git checkout-index --stdin --force --prefix="$HOME/.puppetlabs/opt/puppet/cache/puppet-module/" --
+else
+  find skeleton -type f | git checkout-index --stdin --force --prefix="$HOME/.puppet/var/puppet-module/" --
+fi
