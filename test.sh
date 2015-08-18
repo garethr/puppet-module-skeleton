879d8ee6dc7acabe93de8f4fb61135a6ddb501ce
Gareth Rushgrove <gareth@morethanseven.net>
2015-08-18 15:18:24 +0100

    Add rubocop testing for native code

--- test.sh
+++ test.sh
@@ -9,4 +9,4 @@ fi
 export BUNDLE_GEMFILE=./Gemfile
 bundle install --without system_tests development
 # Not running metadata checks as module generate uses invalid license string
-bundle exec rake lint syntax spec
+bundle exec rake lint syntax rubocop spec
