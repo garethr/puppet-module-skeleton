0e15cffda28259c522e5e78e73f4646a9f780111
Gareth Rushgrove <gareth@morethanseven.net>
2015-08-18 12:57:00 +0100

    Appears to be a change of behaviour between Puppet 4 and 3

--- test.sh
+++ test.sh
@@ -1,5 +1,10 @@
 #!/bin/bash -xv
-cd user-module
+# Change in behaviour between Puppet versions
+if [ $(puppet --version |sed -e 's/\..*$//' ) -gt 3 ]; then
+  cd module
+else
+  cd user-module
+fi
 # Otherwise we pick up the parent bundle
 export BUNDLE_GEMFILE=./Gemfile
 bundle install --without system_tests development
