25787ac95f0781e9072863a06b2d79a7ad9513ff
Gareth Rushgrove <gareth@morethanseven.net>
2015-08-25 12:38:24 +0100

    Add Ruby code coverage as well as Puppet code coverage

--- skeleton/spec/spec_helper.rb
+++ skeleton/spec/spec_helper.rb
@@ -2,3 +2,15 @@ require 'puppetlabs_spec_helper/module_spec_helper'
 require 'rspec-puppet-facts'
 
 include RspecPuppetFacts
+
+require 'simplecov'
+require 'simplecov-console'
+
+SimpleCov.start do
+  add_filter '/spec'
+  add_filter '/vendor'
+  formatter SimpleCov::Formatter::MultiFormatter[
+    SimpleCov::Formatter::HTMLFormatter,
+    SimpleCov::Formatter::Console
+  ]
+end
