2fcbc39a34de09709b440428e7aba13ca9fb9b07
Robert Rauch <rauchrob@users.noreply.github.com>
2015-11-13 22:01:56 +0100

    Strip .gitkeep files during Installation
    This is an alternate solution to #90.

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 if [ $(puppet --version |sed -e 's/\..*$//' ) -gt 3 ]; then
-  find skeleton -type f | git checkout-index --stdin --force --prefix="$HOME/.puppetlabs/opt/puppet/cache/puppet-module/" --
+  find skeleton -type f -not -name .gitkeep | git checkout-index --stdin --force --prefix="$HOME/.puppetlabs/opt/puppet/cache/puppet-module/" --
 else
-  find skeleton -type f | git checkout-index --stdin --force --prefix="$HOME/.puppet/var/puppet-module/" --
+  find skeleton -type f -not -name .gitkeep | git checkout-index --stdin --force --prefix="$HOME/.puppet/var/puppet-module/" --
 fi
