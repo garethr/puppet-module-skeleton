58918e5f20bc23aa750a1ab64e094c2e48fcbd75
Ben Ford <ben.ford@puppetlabs.com>
2016-02-22 10:46:57 -0800

    Simpler install dir calculation
    This will catch more edge cases and works in at least Puppet 3 & 4.

--- install.sh
+++ install.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-case $(puppet --version) in
-  3*)
-    install_dir="$HOME/.puppet/var/puppet-module/"
-    ;;
-  4*)
-    install_dir="$HOME/.puppetlabs/opt/puppet/cache/puppet-module/"
-    ;;
-esac
+install_dir=$(puppet apply --configprint module_working_dir)
 
 find skeleton -type f -not -name .gitkeep | git checkout-index --stdin --force --prefix=$install_dir
-
