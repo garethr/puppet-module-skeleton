74042b83fc5d819b64d009d335282a3478edd635
Alexander Fisher <alex@linfratech.co.uk>
2016-02-24 16:23:17 +0000

    Fix install.sh
    Skeleton was being deployed to
    ~/.puppetlabs/opt/puppet/cache/puppet-moduleskeleton and not being
    picked up by puppet module generate.

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-install_dir=$(puppet apply --configprint module_working_dir)
+install_dir=$(puppet apply --configprint module_working_dir)/
 
 find skeleton -type f -not -name .gitkeep | git checkout-index --stdin --force --prefix=$install_dir
